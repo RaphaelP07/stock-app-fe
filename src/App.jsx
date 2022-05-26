be7028996eae9f24faaf80233c0a92eae443b2d3
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-26 19:11:15 +0800

    added wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Wallet from "./components/Wallet";
 
 function App() {
   return (
@@ -19,6 +21,12 @@ function App() {
             path="/stock-app-fe/signup"
             element={
               <SignUp/>
+            }
+          />
+          <Route
+            path="/stock-app-fe/wallet"
+            element={
+              <Wallet/>
             }
           />
         </Routes>
