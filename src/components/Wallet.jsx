194d463300906aea0c1d8686deb12f4547a1c934
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-31 10:31:36 +0800

    added design to wallet

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -15,10 +15,7 @@ const Wallet = () => {
         Authorization: token.authorization
       }
     }).then((res) => {
-      console.log("res", res)
       setBalance(res.data.balance)
-    }).catch((err) => {
-      console.log("err", err)
     });
   }, []);
 
@@ -26,7 +23,19 @@ const Wallet = () => {
     <div className='container'>
       <Nav />
       <div className="wallet-container">
-        Wallet balance: ${balance}
+        <div className="balance-container">
+          <p>WALLET BALANCE:</p>
+          <div className="balance">
+            {balance === null ? 'loading...' : `$${Number.parseFloat(balance).toFixed(2)}`}
+          </div>
+        </div>
+        <div className="actions-container">
+          <div className='first action'>CASH IN</div>
+          <div className='action'>CASH OUT</div>
+          <div className='action'>HISTORY</div>
+          <div className='action'>PORTFOLIO</div>
+          <div className='last action'>STOCKS</div>
+        </div>
       </div>
     </div>
   )
