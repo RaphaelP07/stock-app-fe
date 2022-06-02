608432151418095878b6085b682d10b2e3a79602
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-02 20:00:26 +0800

    link and props to components

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import { useState, useEffect } from "react";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Wallet from "./components/Wallet";
+import CashAction from "./components/CashAction";
+import History from "./components/History";
+import Portfolio from "./components/Portfolio";
+import Stocks from "./components/Stocks";
+import Transaction from "./components/Transaction";
+import Stock from "./components/Stock";
 
 function App() {
   const [loggedUser, setLoggedUser] = useState("");
   const [loggedID, setLoggedID] = useState("");
+  const [transactionId, setTransactionId] = useState(0)
+  const [symbol, setSymbol] = useState('')
+  const [action, setAction] = useState('')
 
   return (
     <Router>
@@ -24,18 +33,63 @@ function App() {
               />
             }
           />
-          <Route
-            path="/stock-app-fe/signup"
+          <Route 
+            path="/stock-app-fe/wallet" 
             element={
-              <SignUp/>
+              <Wallet
+              setAction={(action) => setAction(action)}
+              />
             }
           />
-          <Route
-            path="/stock-app-fe/wallet"
+          <Route 
+            path="/stock-app-fe/wallet/cash-in" 
             element={
-              <Wallet/>
-            }
-          />
+              <CashAction
+                action={action}
+              />}
+            />
+          <Route 
+            path="/stock-app-fe/wallet/cash-out" 
+            element={
+              <CashAction
+                action={action}
+              />}
+            />
+          <Route 
+            path="/stock-app-fe/wallet/history" 
+            element={
+              <History
+              setTransactionId={(id) => setTransactionId(id)}
+              />
+            }/>
+          <Route 
+            path="/stock-app-fe/wallet/transaction" 
+            element={
+              <Transaction
+                transactionId={transactionId}
+              />
+            }/>
+          <Route 
+            path="/stock-app-fe/stocks" 
+            element={
+              <Stocks
+                setSymbol={(symbol) => setSymbol(symbol)}
+              />
+            }/>
+          <Route 
+            path="/stock-app-fe/stocks/stock" 
+            element={
+              <Stock
+                symbol={symbol}
+              />
+            }/>
+          <Route path="/stock-app-fe/wallet/portfolio" 
+            element={
+              <Portfolio
+                setSymbol={(symbol) => setSymbol(symbol)}
+              />
+            }/>
+          <Route path="/stock-app-fe/signup" element={<SignUp/>}/>
         </Routes>
       </GlobalProvider>
     </Router>
