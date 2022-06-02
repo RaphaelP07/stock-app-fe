a97d3ca7354124748c08326b02f9771c0eea5dce
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-06-02 20:05:18 +0800

    added table for stocks

--- src/components/Stocks.jsx
+++ src/components/Stocks.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { GlobalContext } from "../context/GlobalState";
+import Nav from "./Nav";
+import axios from "axios";
 
-const Stocks = () => {
+const Stocks = ({ setSymbol }) => {
+  const { baseURL, token } = useContext(GlobalContext);
+  const navigate = useNavigate()
+  const [stocks, setStocks] = useState(null)
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `${baseURL}/stocks/top_ten`,
+      headers: token,
+    }).then((res) => {
+      setStocks(res.data);
+    });
+  }, [])
+
+  const goToStock = (symbol) => {
+    setSymbol(symbol)
+    navigate('/stock-app-fe/stocks/stock')
+  }
+
+  const backToWallet = () => {
+    navigate('/stock-app-fe/wallet')
+  }
+
   return (
-    <div>Stocks</div>
+    <div>
+      <Nav/>
+      <div className="container">
+        <table className='wallet-container'>
+          <thead>
+            <tr className='top-row'>
+              <th>SYMBOL</th>
+              <th>PRICE</th>
+            </tr>
+          </thead>
+          <tbody>
+          {stocks === null ? 
+          <tr>
+            <th colSpan={2}>Loading stocks</th>
+          </tr> :
+          stocks.map(stock => 
+            <tr className='row' onClick={() => goToStock(stock.symbol)} key={stocks.indexOf(stock)} >
+              <th className='light'>
+                {stock.symbol}
+              </th>
+              <th className='light'>
+                ${Number.parseFloat(stock.latestPrice).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </th>
+            </tr>
+            )}
+            <tr className='row'>
+              <th className='back' colSpan='2' onClick={() => backToWallet()}>
+                BACK TO WALLET
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
