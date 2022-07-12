a82cf4ebfe806d6d1d2be150b3d56f1a5466698c
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-07-12 18:34:51 +0800

    changed baseurl for deployment

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -15,7 +15,7 @@ export const GlobalContext = createContext(initialState);
 // provider component
 export const GlobalProvider = ({ children, headers }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-  const baseURL = "http://localhost:3000";
+  const baseURL = "https://raphdev-stock-app-api.herokuapp.com";
 
   //Actions
   function setToken(token) {
